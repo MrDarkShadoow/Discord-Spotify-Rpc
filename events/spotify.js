4a1cc87c86bf12c99923c9eb8a608b67e11c1109
MrDarkShadoow <98183554+MrDarkShadoow@users.noreply.github.com>
2022-01-22 12:53:19 -0300

    Update spotify.js

--- events/spotify.js
+++ events/spotify.js
@@ -4,23 +4,23 @@ module.exports = (client) => {
 
   let presence = rpcGenerator.createSpotifyRpc(client)
 
-    .setDetails("RPC Spotify") // Music Name
+    .setDetails("Music Name") // Music Name
 
-    .setState("DarkShaoow") // Artist
+    .setState("Artist") // Artist
 
-    .setAssetsLargeImage("spotify:ab67616d0000b273d6ca2480c92b6f62c4c3d59e")// Go to Readme.md
+    .setAssetsLargeImage("spotify:")// Go to Readme.md
 
 
     .setStartTimestamp(Date.now()) // remember to use a number WITHOUT QUOTES and an Epoch value https://www.epochconverter.com
 
-    .setEndTimestamp(Date.now()+1672531200000) // remember to use a number WITHOUT QUOTES and an [Epoch value] (https://www.epochconverter.com)
+    .setEndTimestamp(Date.now()) // remember to use a number WITHOUT QUOTES and an [Epoch value] (https://www.epochconverter.com)
 
 
 
   console.log(presence.toDiscord())
 
 
-  client.user.setStatus("idle");
+  client.user.setStatus("online"); //status accont(online, idle and dnd.)
 
 
   client.user.setPresence(presence.toDiscord()).catch(console.error);
