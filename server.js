f7776c62351afb1424483ebb8fefde2f50136cc5
MrDarkShadoow <98183554+MrDarkShadoow@users.noreply.github.com>
2022-01-22 15:03:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express();
 
 server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/mysite.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 function keepAlive(){
     server.listen(process.env.PORT, ()=>{console.log("RPC IS READY")});
