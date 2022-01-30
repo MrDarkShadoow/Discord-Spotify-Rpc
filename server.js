8f371aa1e918cb8de4a8f990f393967ed9ed55a5
MrDarkShadoow <98183554+MrDarkShadoow@users.noreply.github.com>
2022-01-30 14:24:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const server = express();
 
-server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
-function keepAlive(){
-    server.listen(process.env.PORT, ()=>{console.log("RPC IS READY")});
+server.all("/", (req, res) => {
+  res.send('<meta http-equiv="refresh" content="0; URL=https://shadoowlinks.darkshadow39.repl.co"/>')
+})
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Rpc is ready.")
+  })
 }
 module.exports = keepAlive
